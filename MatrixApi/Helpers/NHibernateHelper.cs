c14da3174671e8428c6b1d9c6c763cd1f647e75f
Jack Dehlin <jdehlin@gmail.com>
2013-12-20 10:07:12 -0500

    nhibernate stuff

---
+++ MatrixApi/Helpers/NHibernateHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace MatrixApi.Helpers
+{
+    public sealed class NHibernateHelper
+    {
+        private const string CurrentSessionKey = "nhibernate.current_session";
+        private static readonly ISessionFactory SessionFactory;
+
+        static NHibernateHelper()
+        {
+            SessionFactory = new Configuration().Configure().BuildSessionFactory();
+        }
+
+        public static ISession GetCurrentSession()
+        {
+            var context = HttpContext.Current;
+            var currentSession = context.Items[CurrentSessionKey] as ISession;
+
+            if (currentSession == null)
+            {
+                currentSession = SessionFactory.OpenSession();
+                context.Items[CurrentSessionKey] = currentSession;
+            }
+
+            return currentSession;
+        }
+
+        public static void CloseSession()
+        {
+            var context = HttpContext.Current;
+            var currentSession = context.Items[CurrentSessionKey] as ISession;
+
+            if (currentSession == null)
+            {
+                // No current session
+                return;
+            }
+
+            currentSession.Close();
+            context.Items.Remove(CurrentSessionKey);
+        }
+
+        public static void CloseSessionFactory()
+        {
+            if (SessionFactory != null)
+            {
+                SessionFactory.Close();
+            }
+        }
+    }
+}
